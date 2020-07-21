8bfcfe4d1e64c54eea415bad88d1a42ca5037d4a
96tm <khabcity@gmail.com>
2020-07-21 19:16:12 +0300

    Removed deployment instructions and related fils, edited requirements.txt

--- uninstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker stop simple_messenger
-docker stop postgres-custom
-docker rm simple_messenger
-docker rm postgres-custom
-docker image rm simple_messenger
-docker volume rm DatabaseVolume
